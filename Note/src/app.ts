3cb0bc43cf833f185af7fe1e94629e1736d2b2dd
DhruborajDey <dhruborajdey@gmail.com>
2025-07-09 01:36:03 +0530

    monitoring url

--- Note/src/app.ts
+++ Note/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Request, Response } from "express";
 import cors from "cors";
 import signRouter from "../routes/sign-in";
 import notes from "../routes/notes";
@@ -13,6 +14,9 @@ app.use(
     credentials: true, // if using cookies or Authorization headers
   })
 );
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Server is alive");
+});
 app.use("/otp", signRouter);
 app.use("/notes", notes);
 app.use("/getUser", userRoute);
